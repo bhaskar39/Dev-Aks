53414aedde20d6794c03f5d1b32904eccb75dcb9
Bhaskar Desharaju <bhaskar.desharaju@netenrich.com>
2019-08-03 11:55:40 +0530

    intial commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+module "resource_group" {
+  source = "source"
+  
+}
+
+module "virtual_network" {
+  source = "source"
+  
+}
+
+module "mysql_virtual_machine" {
+  source = "source"
+  
+}
+
+module "kafka_virtual_machine" {
+  source = "source"
+  
+}
+
+module "aka_cluster" {
+  source = "source"
+  
+}
+
+
+
+
+
+
---
+++ outputs.tf
@@ -0,0 +1,22 @@
+output "mysql_ip" {
+  value = "value"
+}
+
+output "kafka_ip" {
+  value = "value"
+}
+
+output "aks_endpoint" {
+  value = "value"
+}
+
+output "aks_cluster_pip" {
+  value = "value"
+}
+
+output "aks_https_end_point" {
+  value = "value"
+}
+
+
+
