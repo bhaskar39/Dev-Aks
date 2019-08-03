3f63634a4108ec89bb33f713172d6061ffb0c157
bhaskar39 <bhaskar39@gmail.com>
2019-08-03 13:05:44 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
-      resource_group_name = "Diva-AKS-Storage-POC"
-      storage_account_name  = "repostrgdevaks"
-      container_name        = "statefiles"
-      access_key = "oudZocVWP5LmmHdKs+U/rwUCGd2s1Lw0WPLVXESz9fUFzmV6qjzDF/cxyplTrRei4iL8DaZ8XNkSS/X2INhQ3A=="
+      resource_group_name = "Da-AKS-age-POC"
+      storage_account_name  = "reposgdevaks"
+      container_name        = "stateles"
+      access_key = "Z8XNkSS/X2INhQ3A=="
       key                   = "Dev/terraform.tfstate"
   }
 }
