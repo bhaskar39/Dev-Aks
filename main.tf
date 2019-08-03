811cf802fc49bf86dfb473e3e189330386bee86c
Bhaskar Desharaju <bhaskar.desharaju@netenrich.com>
2019-08-03 18:45:36 +0530

    rg name

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ module "resource_group" {
 }
 
 module "virtual_network" {
-  source = "github.com/bhaskar39/virtualnet?ref=v1.4"
+  source = "github.com/bhaskar39/virtualnet?ref=v1.5"
 
   resource_group_name       = "${var.resource_group_name}"
   location                  = "${var.location}"
